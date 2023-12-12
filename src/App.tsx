59f624e5aae5a998116ee796c63ea2048e57e215
Nicolas Carnevale <ncarnevaledata@gmail.com>
2023-12-12 12:50:13 -0300

    Cleaned up project start

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+import { Grid } from "@chakra-ui/react";
+// import { useState } from "react";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
-
-  return (
-    <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>Hello Niss</Alert>
-      )}
-      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
-    </div>
-  );
+  return <Grid templateAreas={`"nav nav" "aside main"`}></Grid>;
 }
 
 export default App;
