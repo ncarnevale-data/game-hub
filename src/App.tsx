d700bcae479234a7379863724472c80c9b61201f
Nicolas Carnevale <ncarnevaledata@gmail.com>
2023-12-12 13:13:57 -0300

    Added Navbar Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 // import { useState } from "react";
 
 function App() {
@@ -9,8 +10,8 @@ function App() {
         lg: `"nav nav" "aside main"`, // >1024px
       }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg="gold">
