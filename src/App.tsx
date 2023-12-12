fb399d0beed263c86c52168f7804852dde0a0430
Nicolas Carnevale <ncarnevaledata@gmail.com>
2023-12-12 12:57:21 -0300

    Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import { Grid } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 // import { useState } from "react";
 
 function App() {
-  return <Grid templateAreas={`"nav nav" "aside main"`}></Grid>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, // >1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
