a9268115d80fb2e11be3851ed7b5f9cbef86788f
Nicolas Carnevale <ncarnevaledata@gmail.com>
2024-01-17 16:39:44 -0300

    Implement infinite queries with load more button

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
@@ -16,16 +16,24 @@ export interface Game {
 
 
 const useGames = (gameQuery: GameQuery) => 
-  useQuery<FetchResponse<Game>, Error>({
+  useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ['games', gameQuery],
-    queryFn: () => apiClient.getAll ({
+    queryFn: ({ pageParam }) => apiClient.getAll ({
       params:{ 
         genres: gameQuery.genre?.id, 
         parent_platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
-      }
-  })
+        search: gameQuery.searchText,
+        page: pageParam
+      },
+      
+  }),
+  initialPageParam: 1,
+  getNextPageParam: (lastPage, allPages) => {
+    return lastPage.next ? allPages.length + 1 : undefined;
+  },
+  staleTime: 24 * 60 * 60 * 1000 //24h
+
 })
 
 export default useGames
