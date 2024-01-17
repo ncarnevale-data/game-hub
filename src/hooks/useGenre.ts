5a328811f09f4b393b6cd3690d19c87d725756ee
Nicolas Carnevale <ncarnevaledata@gmail.com>
2024-01-17 17:26:09 -0300

    add lookup hooks

---
+++ src/hooks/useGenre.ts
@@ -0,0 +1,10 @@
+import useGenres from "./useGenres";
+
+const useGenre = (id?:number) => {
+    const { data: genres } = useGenres();
+    return genres?.results.find(
+      (p) => p.id === id
+    );
+}
+
+export default useGenre
