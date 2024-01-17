0d041bc17714660a25d59a6b842c22561941b4ff
Nicolas Carnevale <ncarnevaledata@gmail.com>
2024-01-17 15:36:11 -0300

    Fetch Platforms with React Query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -11,7 +11,7 @@ export interface Genre {
 
 const useGenres= () => useQuery({
     queryKey: ['genres'],
-    queryFn: () => apiClient.get<FetchResponse<Genre>>('genres').then(res => res.data),
+    queryFn: () => apiClient.get<FetchResponse<Genre>>('/genres').then(res => res.data),
     staleTime: 24 * 60 * 60 * 1000, //24h
     initialData: { count: genres.length, results: genres }
 })
