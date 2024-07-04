f222265757b22db89a9affcde2ba382f56218ebb
Nicolas Carnevale <ncarnevaledata@gmail.com>
2024-07-04 10:48:23 -0300

    Change OR for nullish coalescing operator

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -21,7 +21,7 @@ const SortSelector = () => {
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Order by: {currentSortOrder?.label || "Relevance"}
+        Order by: {currentSortOrder?.label ?? "Relevance"}
       </MenuButton>
       <MenuList>
         {sortOrders.map((order) => (
