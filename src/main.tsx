8e15a8781a2484562eaef3dd2ee80a875fb81951
Nicolas Carnevale <ncarnevaledata@gmail.com>
2024-07-04 10:39:33 -0300

    Remove index.css

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import theme from "./theme";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
-// import './index.css'
 
 const queryClient = new QueryClient();
 
