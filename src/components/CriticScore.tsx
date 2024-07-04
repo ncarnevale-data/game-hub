79e89c1f4aa53a977c7857d8888ca76ae54358ec
Nicolas Carnevale <ncarnevaledata@gmail.com>
2024-07-04 10:45:43 -0300

    Extrat nested ternary operation for readability

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -5,7 +5,16 @@ interface Props {
 }
 
 export const CriticScore = ({ score }: Props) => {
-  let color = score > 75 ? "green" : score > 60 ? "yellow" : "red";
+  let color: string;
+
+  if (score > 75) {
+    color = "green";
+  } else if (score > 60) {
+    color = "yellow";
+  } else {
+    color = "red";
+  }
+
   return (
     <Badge colorScheme={color} fontSize="14px" px={2} borderRadius="4px">
       {score}
